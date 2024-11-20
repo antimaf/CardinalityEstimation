053da1a70356026be79af3b6ef74ab5ab1c58958
santzjr <anthonyimafidon24@gmail.com>
2024-11-20 20:57:49 +0000

    Fix xxhash: Separate declaration and implementation, add missing headers

--- CardinalityEstimation/third_party/xxhash/xxhash.c
+++ CardinalityEstimation/third_party/xxhash/xxhash.c
@@ -1,3 +1,38 @@
-#define XXH_PRIVATE_API
-#define XXH_IMPLEMENTATION
+/*
+ * xxHash - Extremely Fast Hash algorithm
+ * Implementation File
+ * Copyright (C) 2012-2020 Yann Collet
+ *
+ * BSD 2-Clause License (https://www.opensource.org/licenses/bsd-license.php)
+ */
+
 #include "xxhash.h"
+#include <string.h>
+
+uint64_t XXHash64(const void* input, size_t length, uint64_t seed) {
+    const unsigned char* p = (const unsigned char*)input;
+    uint64_t hash = seed + 0x9E3779B97F4A7C15ULL;
+    
+    while (length >= 8) {
+        uint64_t k;
+        memcpy(&k, p, sizeof(k));  // Safe unaligned read
+        k *= 0x9DDFEA08EB382D69ULL;
+        k ^= k >> 47;
+        k *= 0x9DDFEA08EB382D69ULL;
+        hash ^= k;
+        hash *= 0x9DDFEA08EB382D69ULL;
+        p += 8;
+        length -= 8;
+    }
+    
+    while (length--) {
+        hash ^= *p++;
+        hash *= 0x9DDFEA08EB382D69ULL;
+    }
+    
+    hash ^= hash >> 47;
+    hash *= 0x9DDFEA08EB382D69ULL;
+    hash ^= hash >> 47;
+    
+    return hash;
+}
--- CardinalityEstimation/third_party/xxhash/xxhash.h
+++ CardinalityEstimation/third_party/xxhash/xxhash.h
@@ -10,34 +10,17 @@
 #define XXHASH_H_5627135585666179
 
 #include <stdint.h>
+#include <stddef.h>
 
-// XXHash implementation
-uint64_t XXHash64(const void* input, size_t length, uint64_t seed) {
-    // Simple implementation for now
-    const unsigned char* p = (const unsigned char*)input;
-    uint64_t hash = seed + 0x9E3779B97F4A7C15ULL;
-    
-    while (length >= 8) {
-        uint64_t k = *(uint64_t*)p;
-        k *= 0x9DDFEA08EB382D69ULL;
-        k ^= k >> 47;
-        k *= 0x9DDFEA08EB382D69ULL;
-        hash ^= k;
-        hash *= 0x9DDFEA08EB382D69ULL;
-        p += 8;
-        length -= 8;
-    }
-    
-    while (length--) {
-        hash ^= *p++;
-        hash *= 0x9DDFEA08EB382D69ULL;
-    }
-    
-    hash ^= hash >> 47;
-    hash *= 0x9DDFEA08EB382D69ULL;
-    hash ^= hash >> 47;
-    
-    return hash;
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// XXHash function declaration
+uint64_t XXHash64(const void* input, size_t length, uint64_t seed);
+
+#ifdef __cplusplus
 }
+#endif
 
 #endif // XXHASH_H_5627135585666179
